0a1b12966a5c113149fc8ca863dbcfc7ea23af44
Dan McManus <mcmanusdbm@gmail.com>
2016-01-19 13:15:53 -0600

    re up commit

--- TopSecretPlans/HyperDrive.cs
+++ TopSecretPlans/HyperDrive.cs
@@ -8,7 +8,41 @@ namespace TopSecretPlans
 {
     class HyperDrive
     {
+		public string destination;
+        public int fuelCellLevel;
+        public int gravityLevel;
+        public bool hyperDriveExecution;
+        public HyperDrive(string destination, int fuelCellLevel, int GraveityLevel, bool hyperDriveExecution)
+        {
+           
+
+        }
+        public bool IsHyperDriveExecuted()
+        {
+            if(hyperDriveExecution==true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void UseHyperDrive()
+        {
+            if(fuelCellLevel>0)
+            {
+                this.hyperDriveExecution = true;
+                this.fuelCellLevel--;
+                this.gravityLevel = 0;
+            }
+           else
+            {
+                this.hyperDriveExecution = false;
+                Console.WriteLine("your fuel Cell Level is " + this.fuelCellLevel);
+            }
+        }
     }
-//hyperdrive overbridge reactorcore powercell
+
 
 }
--- TopSecretPlans/OverBridge.cs
+++ TopSecretPlans/OverBridge.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace TopSecretPlans
 {
+
     class OverBridge
+
+
     {
+
     }
 }
