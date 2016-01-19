6792d85dc326ebd5aeadafe86468c8b1d91a8e3b
Adam <adamhool414@gmail.com>
2016-01-19 12:28:04 -0600

    updated hyperdrive

--- TopSecretPlans/TopSecretPlans/HyperDrive.cs
+++ TopSecretPlans/TopSecretPlans/HyperDrive.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace TopSecretPlans
 {
-    class HyperDrive
+    public class HyperDrive
     {
+        public string destination;
+        public int fuelCellLevel;
+        public int gravityLevel;
+        public bool hyperDriveExecution;
+        public HyperDrive(string destination, int fuelCellLevel, int GraveityLevel, bool hyperDriveExecution)
+        {
+           
+
+        }
+        public bool IsHyperDriveExecuted()
+        {
+            if(hyperDriveExecution==true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void UseHyperDrive()
+        {
+            if(fuelCellLevel>0)
+            {
+                this.hyperDriveExecution = true;
+                this.fuelCellLevel--;
+                this.gravityLevel = 0;
+            }
+           else
+            {
+                this.hyperDriveExecution = false;
+                Console.WriteLine("your fuel Cell Level is " + this.fuelCellLevel);
+            }
+        }
     }
 }
