0ca999d31bf19e2136a9930c7cc0af715ed6810f
Dan McManus <mcmanusdbm@gmail.com>
2016-01-19 12:17:25 -0600

    initial commit

---
+++ TopSecretPlans/ReactorCore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSecretPlans
+{
+    class ReactorCore
+    {
+        public int Temperature;
+        int fuelRods;
+        int fuelRodLevel;
+        int controlRods;
+        int controlRodLevel;
+        int waterReactorLevel;
+        int uraniumAmount;
+        int boronAmount;
+
+        public ReactorCore(int FuelRods, int FuelRodLevel, int ControlRods, int ControlRodLevel, int WaterReactorLevel, int UraniumAmount, int BoronAmount)
+        {
+
+        }
+    }
+}
